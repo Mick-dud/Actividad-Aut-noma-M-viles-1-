2312b0645ee011ea6bbc89fab3377a4aeb97b65a
Mick-dud <mba.arevalosabando@gmail.com>
2025-07-24 13:59:22 -0500

    cambio del tamaño de letra

--- src/Ejercicios/EjercicioTres.tsx
+++ src/Ejercicios/EjercicioTres.tsx
@@ -9,7 +9,7 @@ const arr: number[] = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
 
     return (
         <div>
-            <h3>Ejercicio Tres</h3>
+            <h3>Ejercicio 3</h3>
             <ul>
                 {resultadoDividido.map((valor, index) => (
                     <li key={index}>Valor dividido: {valor}</li>
